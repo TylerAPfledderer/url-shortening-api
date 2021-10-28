c9d3d612daf3e495f40e65b3805dfece9d35c981
Tyler Pfledderer <tyler.pfledderer@gmail.com>
2021-10-27 23:30:25 -0400

    feat(LinkButton.vue): Create LinkButton component.
    - Build component using the anchor tag and the LinkButton classes.
    - Create LinkButton variant classes to explicitly associate certain style sets like padding to prop values (i.e. "LinkButton--small" with smaller padding values vs. "LinkButton--large")
    
    This component will be used for the navigation links, the CTA buttons and the Shortener submit button.

---
+++ src/components/LinkButton.vue
@@ -0,0 +1,63 @@
+<template lang="">
+  <a href="#" class="LinkButton" :class="[solidVariant && 'LinkButton--solid', `LinkButton--${size}`]">{{ name }}</a>
+</template>
+<script setup>
+import { defineProps } from 'vue';
+defineProps({
+  /** The visible text of the link */
+  name: {
+    type: String,
+    required: true,
+  },
+  /** Generates the LinkButton variant class with the solid Cyan background */
+  solidVariant: {
+    type: Boolean,
+    default: false,
+  },
+  /** Generate specific padding and connect this to the  */
+  size: {
+    type: String,
+    validator: (v) => ['small', 'large'].includes(v),
+    default: 'small',
+  },
+});
+</script>
+<style scoped>
+.LinkButton {
+  font-weight: var(--font-weight-bold);
+  text-decoration: none;
+  text-align: center;
+}
+
+.LinkButton:hover,
+.LinkButton:focus {
+  color: black;
+}
+
+.LinkButton--solid {
+  background: var(--primary-cyan);
+  border-radius: 28px;
+  color: white !important;
+}
+
+.LinkButton--solid:hover,
+.LinkButton--solid:focus {
+  text-decoration: none !important;
+  filter: brightness(1.1);
+  outline: 2px solid white;
+}
+
+.LinkButton--large {
+  padding: 12px 40px;
+}
+
+.LinkButton--small {
+  padding: 8px 24px;
+}
+
+@media (min-width: 768px) {
+  .LinkButton:not(.LinkButton--solid) {
+    padding: 0;
+  }
+}
+</style>
