995aa1c756532e24b3e21744722a49d17d1f2c9d
Tyler Pfledderer <tyler.pfledderer@gmail.com>
2021-11-05 15:30:41 -0400

    refactor: Create BoostCTA component from the Main, including related imports and styles.

---
+++ src/components/Main/BoostCta.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="BoostCtaSection background-pattern">
+    <h3>Boost your links today</h3>
+    <LinkButton name="Get Started" solidVariant size="large" />
+  </section>
+</template>
+<script setup>
+import LinkButton from '../LinkButton.vue';
+</script>
+<style scoped>
+.BoostCtaSection {
+  color: white;
+  display: flex;
+  flex-direction: column;
+  height: 300px;
+  justify-content: center;
+  align-items: center;
+}
+
+.BoostCtaSection.background-pattern {
+  background-image: url('~@/assets/bg-boost-mobile.svg');
+  background-position: center right;
+  background-size: 100% 100%;
+}
+
+@media (min-width: 768px) {
+  .BoostCtaSection.background-pattern {
+    background-image: url('~@/assets/bg-boost-desktop.svg');
+  }
+}
+</style>
