b97c8c08013df5c3b8b6ec5747996e39eb716d3f
Tyler Pfledderer <tyler.pfledderer@gmail.com>
2021-11-28 19:27:04 -0500

    feat(SingleResult): Build SingleResult component that renders an input url and a successfully generated short url, including the functionlity to copy the shortened url.

---
+++ src/components/Main/ShortenerResults/SingleResult.vue
@@ -0,0 +1,118 @@
+<template>
+  <li class="GeneratedUrlGroup">
+    <div>
+      <span>{{ inputUrl }}</span>
+    </div>
+    <div>
+      <span>{{ generatedUrl }}</span>
+      <button
+        type="button"
+        class="GeneratedUrlGroup__copy"
+        :class="{ 'is-copied': isCopied }"
+        @click="handleCopyUrl(generatedUrl)"
+        :aria-pressed="isCopied"
+      >
+        {{ isCopied ? 'Copied!' : 'Copy' }}
+      </button>
+    </div>
+  </li>
+</template>
+<script setup>
+import { ref, defineProps } from 'vue';
+
+defineProps({
+  inputUrl: {
+    type: String,
+    required: true,
+  },
+  generatedUrl: {
+    type: String,
+    required: true,
+  },
+});
+
+const isCopied = ref(false);
+
+async function handleCopyUrl(url) {
+  isCopied.value = false;
+  const clipboard = navigator.clipboard;
+
+  await clipboard.writeText(url).then(() => (isCopied.value = true));
+}
+</script>
+<style scoped>
+.GeneratedUrlGroup,
+.GeneratedUrlGroup > *:last-child {
+  display: flex;
+  flex-direction: column;
+}
+
+.GeneratedUrlGroup {
+  background: white;
+  border-radius: 5px;
+  margin-top: 24px;
+}
+
+.GeneratedUrlGroup > * {
+  /* Initialize flex-basis primarily for larger screens */
+  flex-basis: 100%;
+  padding: 16px 14px;
+  margin: 0 2px;
+}
+
+.GeneratedUrlGroup > *:first-child {
+  border-bottom: 2px solid var(--neutral-500);
+}
+
+.GeneratedUrlGroup > *:last-child {
+  gap: 12px 24px;
+}
+
+.GeneratedUrlGroup > *:last-child span {
+  color: var(--primary-cyan);
+}
+
+.GeneratedUrlGroup__copy {
+  border-radius: 5px;
+  border: none;
+  height: 40px;
+  background: var(--primary-cyan);
+  color: white;
+  cursor: pointer;
+  font-family: inherit;
+  font-size: 16px;
+  font-weight: var(--font-weight-bold);
+}
+
+.GeneratedUrlGroup__copy:hover,
+.GeneratedUrlGroup__copy:focus {
+  filter: brightness(1.1);
+  outline: 2px solid white;
+}
+
+.GeneratedUrlGroup__copy.is-copied {
+  background: var(--primary-violet);
+  color: white;
+  transition: background 0.2s ease-in-out, color 0.2s ease-in-out;
+}
+
+@media (min-width: 1024px) {
+  .GeneratedUrlGroup,
+  .GeneratedUrlGroup > *:last-child {
+    flex-direction: row;
+    align-items: center;
+  }
+
+  .GeneratedUrlGroup > *:first-child {
+    border-bottom: 0;
+  }
+
+  .GeneratedUrlGroup > *:last-child {
+    justify-content: flex-end;
+  }
+
+  .GeneratedUrlGroup__copy {
+    width: 96px;
+  }
+}
+</style>
