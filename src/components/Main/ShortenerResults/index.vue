96d97262174c08f774e53b73628d8ada43c687eb
Tyler Pfledderer <tyler.pfledderer@gmail.com>
2021-11-28 19:28:09 -0500

    feat(ShortenerResults.vue): Build the ShortenerResults component that displays the list of the successfully generated urls.

---
+++ src/components/Main/ShortenerResults/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <section
+    v-if="generatedUrls"
+    style="background: var(--neutral-500);"
+    :style="`padding: ${hasApiErr ? '108px' : '72px'} var(--base-padding-x) 0;`"
+  >
+    <DesktopWrapper>
+      <ul style="width: 100%;">
+        <SingleResult v-for="(urlPair, index) in generatedUrls" :key="index" v-bind="urlPair" />
+      </ul>
+    </DesktopWrapper>
+  </section>
+</template>
+<script setup>
+import { computed } from 'vue';
+import { useStore } from 'vuex';
+import DesktopWrapper from '../../DesktopWrapper.vue';
+import SingleResult from './SingleResult.vue';
+
+const { state } = useStore();
+
+const generatedUrls = computed(() => state.generatedUrls);
+const hasApiErr = computed(() => state.hasServerErr);
+</script>
