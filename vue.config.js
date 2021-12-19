3dc0da58b76c74fae41c06fcf2f390531a2029b6
Tyler Pfledderer <tyler.pfledderer@gmail.com>
2021-12-19 16:45:36 -0500

    chore: Update packge.json name to "url-shortening-api" from "uri-..." and add an index apge entry point option in the Vue Config so that the title of the project page in the browser tab can be set to "URL Shortening API"

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '/url-shortening-api/' : '/',
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      title: 'URL Shortening API',
+    },
+  },
 };
